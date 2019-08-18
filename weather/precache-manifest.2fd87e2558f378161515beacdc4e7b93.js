543cb7be6757ebc79ceaf6eeaa04434c16e40398
bzrov <bzrov@mail.ru>
2019-08-18 21:12:05 +0300

    weather app upload

---
+++ weather/precache-manifest.2fd87e2558f378161515beacdc4e7b93.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ac0c73f649b02b1137cbee7ceafde38",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "7cee55cc89d687e85375",
+    "url": "/weather/static/css/main.bf906d90.chunk.css"
+  },
+  {
+    "revision": "80e079b7d49502a03897",
+    "url": "/weather/static/js/2.77161544.chunk.js"
+  },
+  {
+    "revision": "7cee55cc89d687e85375",
+    "url": "/weather/static/js/main.c60803d0.chunk.js"
+  },
+  {
+    "revision": "3cb3cde5c978189545b4",
+    "url": "/weather/static/js/runtime~main.6cff1e35.js"
+  },
+  {
+    "revision": "8a7526f94b235a66d656b9b1dcb401a2",
+    "url": "/weather/static/media/cloud.8a7526f9.png"
+  },
+  {
+    "revision": "7ead19471cc71dc339af34fa91de9c92",
+    "url": "/weather/static/media/rain.7ead1947.png"
+  }
+]);
