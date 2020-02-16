c1abda15025b6ae3818f23dc36e80d21d2cb7f92
bzrov <bzrov@mail.ru>
2020-02-16 22:17:41 +0300

    init gis-games

---
+++ gisgames/js/main.js
@@ -0,0 +1,6 @@
+$(".header__authorization").click(function() {
+    $(".authorization").addClass("authorization_active")
+});
+$(".authorization__btn-close").click(function() {
+    $(".authorization").removeClass("authorization_active")
+});
