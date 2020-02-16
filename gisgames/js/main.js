12497b1c481810d7bdb510c7c35d49c0a2bb6f09
bzrov <bzrov@mail.ru>
2020-02-17 00:40:59 +0300

    fix scroll,fix  popup scroll

--- gisgames/js/main.js
+++ gisgames/js/main.js
@@ -1,6 +1,9 @@
 $(".header__authorization").click(function() {
     $(".authorization").addClass("authorization_active")
+    $("html").css({'overflow' : 'hidden',})
+
 });
 $(".authorization__btn-close").click(function() {
     $(".authorization").removeClass("authorization_active")
+    $("html").css({'overflow-x' : 'hidden','overflow-y' : 'auto'})
 });
