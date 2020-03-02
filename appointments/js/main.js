d5dec0e876e9cbf125d707eb07b7797bb7a6e1e4
bzrov <bzrov@mail.ru>
2020-03-03 01:35:05 +0300

    update appointments

--- appointments/js/main.js
+++ appointments/js/main.js
@@ -751,7 +751,3 @@ window.addEventListener('click', function(event) {
     calendar.classList.remove('visible');
   }
 });
-const isSafari = /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || (typeof safari !== 'undefined' && safari.pushNotification));
-if(isSafari){
-  document.body.classList.add('safari')
-}
