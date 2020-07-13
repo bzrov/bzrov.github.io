fdc067fdf2ed28cf9f7e2585f55b0cc3fd5b98ad
bzrov <bzrov@mail.ru>
2020-07-13 04:39:47 +0300

    upload accounting

---
+++ accounting/precache-manifest.0f8aa8284e199c3297809a5470f02674.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9cba877b251bfe3deb96893598cfa1e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "62bf1e16f183dda0f112",
+    "url": "/static/css/main.bd330e0d.chunk.css"
+  },
+  {
+    "revision": "6060200af8ae428d0e57",
+    "url": "/static/js/2.be2583cb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.be2583cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62bf1e16f183dda0f112",
+    "url": "/static/js/main.77395ded.chunk.js"
+  },
+  {
+    "revision": "ca2bcbe5f97012261781",
+    "url": "/static/js/runtime-main.5e3879a0.js"
+  },
+  {
+    "revision": "bf80c094626ee44af80267dfde4ff420",
+    "url": "/static/media/boy.bf80c094.mp4"
+  },
+  {
+    "revision": "f2380a29d86afc747183a4ef1f97cbaa",
+    "url": "/static/media/cat.f2380a29.svg"
+  },
+  {
+    "revision": "85663da27db94bfed856b3746d83329d",
+    "url": "/static/media/dog.85663da2.svg"
+  },
+  {
+    "revision": "ec0401f8a7d9c8f94105e71869f58a63",
+    "url": "/static/media/fox.ec0401f8.svg"
+  },
+  {
+    "revision": "5cced49d14a02f93db5e1cb90bbea6d1",
+    "url": "/static/media/koala.5cced49d.svg"
+  },
+  {
+    "revision": "3c6574b3d824f91dc49e4c2e94a95740",
+    "url": "/static/media/lion.3c6574b3.svg"
+  },
+  {
+    "revision": "1d9a953741616937b0bb583f57f0a941",
+    "url": "/static/media/owl.1d9a9537.svg"
+  },
+  {
+    "revision": "8e2badb79083647b76740e0f3504915a",
+    "url": "/static/media/penguin.8e2badb7.svg"
+  },
+  {
+    "revision": "296123ee21f7e15e782f0f100100f51e",
+    "url": "/static/media/pig.296123ee.svg"
+  },
+  {
+    "revision": "7c68f01420eaf9261115bfb76591d3a1",
+    "url": "/static/media/raccoon.7c68f014.svg"
+  },
+  {
+    "revision": "11a8f47e01fe816b8ef48fd9fd3bb810",
+    "url": "/static/media/react.11a8f47e.svg"
+  },
+  {
+    "revision": "2e62bc7b325fee3c5ca65a1d0e1c8c2a",
+    "url": "/static/media/sheep.2e62bc7b.svg"
+  },
+  {
+    "revision": "93fec09c9929ab21a395f2fd81b8b041",
+    "url": "/static/media/shoe.93fec09c.mp4"
+  }
+]);
--- gisgames/js/main.js
+++
@@ -1,9 +0,0 @@
-$(".header__authorization").click(function() {
-    $(".authorization").addClass("authorization_active")
-    $("html").css({'overflow' : 'hidden',})
-
-});
-$(".authorization__btn-close").click(function() {
-    $(".authorization").removeClass("authorization_active")
-    $("html").css({'overflow-x' : 'hidden','overflow-y' : 'auto'})
-});
